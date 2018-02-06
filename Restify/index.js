b2d1e51cf7f9bc51835d312a9bacffc6a6d277b0
InsaneNaman <namangupta.cse@gmail.com>
2018-02-06 19:10:15 +0530

    initial commit

---
+++ Restify/index.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+
+const restify = require('restify');
+const server = restify.createServer();
+
+server.get('/',
+    function (req, res, next) {
+        console.log('I am function first');
+        return next();   
+
+    }, function (req, res ,next) {
+        console.log(' I am function second ');
+        res.send('Response by function 2');
+        return next();
+    }
+);
+
+
+server.listen(process.env.PORT || process.env.port, function () {
+    console.log('Server is running %s , $s ', server.url, server.name);
+
+});
